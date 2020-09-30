3004b9d0db3ce9aa0b76d93e3af86a966d9d315e
Rahel <gabrielaruntu@gmail.com>
2020-09-30 13:09:06 +0700

    lala

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -6,8 +6,8 @@ class Footer extends Component {
     render() { 
         return ( 
             <MDBFooter className="pt-4 mt-4">
-                <MDBContainer fluid className="text-center text-md-left text-white" style={{height:'500px', backgroundColor:'#000000'}}>
-                    <MDBRow>
+                <MDBContainer fluid className="text-center text-md-left text-white" style={{height:'400px', backgroundColor:'#000000'}}>
+                    <MDBRow justify-content='space-between'>
                         <MDBCol md="6"><br/>
                             <h1 className="title text-white" style={{fontSize:'40px'}} >Z A L O R A</h1><br></br>
                             <p>
@@ -16,15 +16,48 @@ class Footer extends Component {
                         </MDBCol>
                     </MDBRow>
                     <MDBRow>
-                        <a className="btn-floating btn-lg btn-fb" type="button" role="button"><i className="fab fa-facebook-f"></i></a>
-                        <a className="btn-floating btn-lg btn-tw" type="button" role="button"><i className="fab fa-twitter"></i></a>
-                        <a className="btn-floating btn-lg btn-gplus" type="button" role="button"><i className="fab fa-google-plus-g"></i></a>
+                        <MDBCol md="6"><br/><br/>
+                            <h5 className="title">LAYANAN</h5>
+                            <ul>
+                            <li className="list-unstyled">
+                                <a href="#!">Bantuan</a>
+                            </li>
+                            <li className="list-unstyled">
+                                <a href="#!">Hubungi Kami</a>
+                            </li>
+                            <li className="list-unstyled">
+                                <a href="#!">Konfirmasi Transfer</a>
+                            </li>
+                            <li className="list-unstyled">
+                                <a href="#!">Status Order</a>
+                            </li>
+                            </ul>
+                        </MDBCol>
+                        <MDBCol md="6"><br/><br/>
+                            <h5 className="title">TENTANG KAMI</h5>
+                            <ul>
+                            <li className="list-unstyled">
+                                <a href="#!">About Us</a>
+                            </li>
+                            <li className="list-unstyled">
+                                <a href="#!">Persyaratan dan Ketentuan</a>
+                            </li>
+                            </ul>
+                        </MDBCol>
+                    </MDBRow>
+                    <MDBRow>
+                        <a href="https://www.facebook.com/"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                        <a href="https://www.instagram.com/?hl=en"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                        <a href="https://www.pinterest.com/"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a>
+                        <a href="https://twitter.com/?lang=en"><i class="fa fa-twitter" aria-hidden="true"></i></a>
                     </MDBRow>
                 </MDBContainer>
+                
             </MDBFooter>
          );
     }
-}
+}        
+            
  
 export default Footer;
     
